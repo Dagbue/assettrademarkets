42a33fac0f2606f3dbe59d41c8a2f72f76c16716
MAC <mac@MACs-MacBook-Pro-3.local>
2024-11-30 16:32:15 +0100

    twentysixth push

--- public/config/app.config.js
+++ public/config/app.config.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
 
   window.__env.api = {
-    baseUrl: "https://investo-super-app-57e7p.ondigitalocean.app/",
+    baseUrl: "https://superdup-app-bngfz.ondigitalocean.app/",
     uploadS3BaseUrl: "https://m2nz1o078e.execute-api.us-east-1.amazonaws.com/prod/",
     timeout: 80000,
   };
